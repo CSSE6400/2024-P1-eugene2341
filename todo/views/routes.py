80d5ca339ea88e6193b7678a637f7767ee89d561
Eugene <eugene@d-i89-192-76.student.eduroam.uq.edu.au>
2024-02-23 14:43:51 +1000

    ok1

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -31,7 +31,7 @@ def get_todo(id):
 
 @api.route('/todos', methods=['POST'])
 def create_todo():
-    return jsonify({
+    return jsonify( {
     "id": 1,
     "title": "Watch CSSE6400 Lecture",
     "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
@@ -40,7 +40,7 @@ def create_todo():
     "deadline_at": "2023-02-27T00:00:00",
     "created_at": "2023-02-20T00:00:00",
     "updated_at": "2023-02-20T00:00:00"
-    }, 201)
+    }), 201
 
 @api.route('/todos/<int:id>', methods=['PUT'])
 def update_todo(id):
